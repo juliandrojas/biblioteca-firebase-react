c28184e6bb7c35d379b0c0135547ea3a1b429885
Julian Rojas <juliandra140201@gmail.com>
2024-02-03 16:12:23 -0500

    Add("Firestore database configuration")

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,27 +1,25 @@
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
-import { getAuth } from 'firebase/auth';
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDbNPLs6cmK_UBqNf9ktRGn3BYMl_YmiVg",
-  authDomain: "biblioteca-react-eb11f.firebaseapp.com",
-  databaseURL: "https://biblioteca-react-eb11f-default-rtdb.firebaseio.com",
-  projectId: "biblioteca-react-eb11f",
-  storageBucket: "biblioteca-react-eb11f.appspot.com",
-  messagingSenderId: "949378375942",
-  appId: "1:949378375942:web:3040cfb86e57c8b6d47e87",
-  measurementId: "G-W3JFYF2XV4"
+  apiKey: "AIzaSyAjK5RQDnj3RCVy9G6cBZ4mcG1FtQYqHgU",
+  authDomain: "biblioteca-react-c163c.firebaseapp.com",
+  projectId: "biblioteca-react-c163c",
+  storageBucket: "biblioteca-react-c163c.appspot.com",
+  messagingSenderId: "486352192282",
+  appId: "1:486352192282:web:fb0936facfdbe71ff222a3"
 };
 
-// Inicializa Firebase
-export const app = initializeApp(firebaseConfig);
-
-const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // Exporta la instancia de autenticación de Firebase
 export const auth = getAuth(app);
+
+// Exporta la instancia de Cloud Firestore
+export const db = getFirestore();
